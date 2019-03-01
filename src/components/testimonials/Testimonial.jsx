57acd61eca31d00a7679465e8b8deec7ae74b527
Karim Ould Mahieddine <karimouldmahieddine@karims-mbp.speedport_w_724v_09011603_05_017>
2019-03-01 23:14:29 +0100

    added css mixins, bootstrap grid ONLY, added own style to bootstrap grid, added testimonial component

---
+++ src/components/testimonials/Testimonial.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './testimonial.scss';
+
+const Testimonial = (titel, text, img) => (
+  <div className="container testimonials-container">
+    <div className="row">
+      <div className="col-xs-12 col-sm-6 col-lg-7" />
+      <div className="col-xs-12 col-sm-6 col-lg-5 testimonial-content">
+        <div className="testimaonial-title">
+          <h2>Wir haben es mit Faber geschafft: Unsere Referenzen.</h2>
+        </div>
+        <div className="testimonial-text">
+          <p>
+            Erklärungstext wieso man bei euch dann essen kann, wenn immer man
+            will. Die Ernährungsberatung ist eine auf Dich abgestimmte Beratung.
+            Das heißt konkret: Wir passen uns auf Dich an und nicht Du auf uns.
+            Du sagst uns was Du willst und wir sagen Dir nicht was Du musst.{' '}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Testimonial;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import Layout from '../components/layouts/Layout';
+import Testimonial from '../components/testimonials/Testimonial';
 
 const IndexPage = props => {
   const { data: home } = props;
@@ -14,19 +15,7 @@ const IndexPage = props => {
         <title> {data.frontmatter.seo_comp.seo_title} </title>
         <meta name="description" content={data.frontmatter.seo_comp.seo_desc} />
       </Helmet>
-      <h1> title: {data.frontmatter.seo_comp.seo_title} </h1>
-      <p> Content: {data.frontmatter.seo_comp.seo_desc} </p>
-      <h2> BlogPosts: </h2>
-      {posts.map(({ node: post }) => (
-        <div key={post.id}>
-          <h3> Blog Post Title: {post.frontmatter.title} </h3>
-          <p> Blog Post Description: {post.frontmatter.description} </p>
-          <p> Blog Post Date: {post.frontmatter.date} </p>
-          <Link to={post.fields.slug} title="link to blog post">
-            Link to blog post{' '}
-          </Link>
-        </div>
-      ))}
+      <Testimonial />
     </Layout>
   );
 };
@@ -55,6 +44,12 @@ export const pageQuery = graphql`
             seo_comp {
               seo_desc
               seo_title
+            }
+            ttestimonial_comp {
+              testimonial {
+                testimonial_text
+                testimonial_title
+              }
             }
           }
         }
