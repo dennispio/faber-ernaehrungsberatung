fd9604e3ccb8b233b15c0c9c232a8c8f07af57ee
dennispio <dennis.piotrowski@web.de>
2019-03-12 14:33:13 +0100

    navibar crossbar, styling changes

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,58 +3,64 @@ import './navigation.scss';
 import { slide as Menu } from 'react-burger-menu';
 import logo from '../../img/FBR-logo-sw.svg';
 import logoBurger from '../../img/FBR-logo-sw1.svg';
+import closeBurgerButton from '../../img/CloseBurgerButton.svg';
+
 
 const Navigation = () => (
   <div className="navigation-container">
-    <div className="image-container">
-      <img alt="LOGO" src={logo} />
-    </div>
-    <div id="menu" className="navigation-menu">
-      <ul>
-        <li>
-          <a href="/"> Start </a>
-        </li>
-        <div className="subnav">
+    <div className="navigation-flex">
+      <div className="image-container">
+        <img alt="LOGO" src={logo} />
+      </div>
+      <div id="menu" className="navigation-menu">
+        <ul>
           <li>
-            <a href="/unsereLeistungen"> Unsere Leistungen</a>
+            <a href="/"> Start </a>
           </li>
-          <div className="triangle" />
-          <div className="subnav-content-container">
-            <div className="subnav-content">
-              <a href="#ernaehrung">Ernährungsbearatung</a>
-              <a href="#coaching">Coaching</a>
-              <a href="#naehrwertanalyse">Nährwertanalyse</a>
-              <a href="#seminare">Seminare und Vorträge</a>
+          <div className="subnav">
+            <li>
+              <a href="/unsereLeistungen"> Unsere Leistungen</a>
+            </li>
+            <div className="triangle" />
+            <div className="subnav-content-container">
+              <div className="subnav-content">
+                <a href="#ernaehrung">Ernährungsbearatung</a>
+                <a href="#coaching">Coaching</a>
+                <a href="#naehrwertanalyse">Nährwertanalyse</a>
+                <a href="#seminare">Seminare und Vorträge</a>
+              </div>
             </div>
           </div>
-        </div>
-        <li>
-          <a href="/referenzen">Referenzen</a>
-        </li>
-        <li>
-          <a href="/ueberuns">Über uns</a>
-        </li>
-        <li>
-          <a href="/blog">Blog</a>
-        </li>
-        <li>
-          <a href="/kontakt">Kontakt</a>
-        </li>
-      </ul>
+          <li>
+            <a href="/referenzen">Referenzen</a>
+          </li>
+          <li>
+            <a href="/ueberuns">Über uns</a>
+          </li>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/kontakt">Kontakt</a>
+          </li>
+        </ul>
+      </div>
     </div>
     <div className="burgerMenu-container">
-      <Menu className="bm-menu" right width={'100%'}>
+      <Menu className="bm-menu" customCrossIcon={<img src={closeBurgerButton} />} right width={'100%'}>
         <div className="burgerMenu-logo">
           <img alt="LOGO" src={logoBurger} />
         </div>
-        <a href="#ernaherung" className="menu-item">Unsere <br className="burgerMenu-br" /> Leistungen</a>
-        <a href="/referenzen" className="menu-item"> Referenzen</a>
-        <a href="/ueberuns" className="menu-item">Über uns</a>
-        <a href="/blog" className="menu-item">Blog</a>
-        <a href="/kontakt" className="menu-item">Kontakt</a>
+        <div className="burgerMenu-content">
+          <a href="#ernaherung" className="menu-item">Unsere <br className="burgerMenu-br" /> Leistungen</a>
+          <a href="/referenzen" className="menu-item">Referenzen</a>
+          <a href="/ueberuns" className="menu-item">Über uns</a>
+          <a href="/blog" className="menu-item">Blog</a>
+          <a href="/kontakt" className="menu-item">Kontakt</a>
+        </div>
       </Menu>
     </div>
-  </div>
+  </div >
 );
 
 export default Navigation;
