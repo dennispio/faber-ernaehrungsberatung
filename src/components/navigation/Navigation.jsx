3694b63d7de74c86def9f896f7335695abe3eaee
dennispio <dennis.piotrowski@web.de>
2019-03-17 23:42:42 +0100

    test fix4

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -8,36 +8,34 @@ import logoBurger from '../../img/FBR-logo-sw1.svg';
 import closeBurgerButton from '../../img/CloseBurgerButton.svg';
 
 const Navigation = ({ pageName }) => (
-  <div className="navigation-container">
-    <div className="burgerMenu-container">
-      <Menu
-        className="bm-menu"
-        customCrossIcon={<img src={closeBurgerButton} alt="close button" />}
-        right
-        width="100%"
-      >
-        <div className="burgerMenu-logo">
-          <img alt="LOGO" src={logoBurger} />
-        </div>
-        <div className="burgerMenu-content">
-          <Link to="leistungen" className="menu-item">
-            Unsere <br className="burgerMenu-br" /> Leistungen
+  <div className="burgerMenu-container">
+    <Menu
+      className="bm-menu"
+      customCrossIcon={<img src={closeBurgerButton} alt="close button" />}
+      right
+      width="100%"
+    >
+      <div className="burgerMenu-logo">
+        <img alt="LOGO" src={logoBurger} />
+      </div>
+      <div className="burgerMenu-content">
+        <Link to="leistungen" className="menu-item">
+          Unsere <br className="burgerMenu-br" /> Leistungen
           </Link>
-          <Link to="/referenzen" className="menu-item">
-            Referenzen
+        <Link to="/referenzen" className="menu-item">
+          Referenzen
           </Link>
-          <Link to="/ueberuns" className="menu-item">
-            Über uns
+        <Link to="/ueberuns" className="menu-item">
+          Über uns
           </Link>
-          <Link to="/blog" className="menu-item">
-            Blog
+        <Link to="/blog" className="menu-item">
+          Blog
           </Link>
-          <Link to="/kontakt" className="menu-item">
-            Kontakt
+        <Link to="/kontakt" className="menu-item">
+          Kontakt
           </Link>
-        </div>
-      </Menu>
-    </div>
+      </div>
+    </Menu>
   </div>
 );
 export default Navigation;
