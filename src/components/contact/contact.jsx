fce5eec3f1f519465a292a0d2a8c40af6ad1529d
dennispio <dennis.piotrowski@web.de>
2019-03-24 10:27:09 +0100

    contact anpassung

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,13 +5,11 @@ import './contact.scss';
 
 const Contact = () => {
     return (
-        <div className="row contact-container">
-            <div className="morph-Container"> <img src={morpheins}></img> </div>
-            <div className="morphzwei-Container"> <img src={morphzwei}></img> </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 contact-heading">
+        <div className="contact-container">
+            <div className="contact-heading">
                 <h1>Kontakt</h1>
             </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 contact-paragraph ">
+            <div className="contact-paragraph ">
                 <p>Ernährungsberatung faber GbR <br />
                     Walsroder Straße 123 B <br />
                     30853 Langenhagen <br /> <br />
@@ -19,7 +17,8 @@ const Contact = () => {
                     Mail: kontakt@ernaehrungsberatung-faber.de <br />
                     Auf Wunsch auch über Skype erreichbar</p>
             </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 button-container">
+
+            <div className="button-container">
                 <div className="button">
                     <a href="mailto:Kontakt@ernaehrungsberatung-faber.de?subject=Ernährungsberatung"><button>E-Mail</button></a>
                 </div>
@@ -27,35 +26,35 @@ const Contact = () => {
                     <a href="tel:+4915785624045"><button>Telefon</button></a>
                 </div>
             </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 form-container">
+            <div className="form-container">
                 <h3>unverbindlich Anfragen</h3>
             </div>
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 form-felder">
+            <div className="form-felder">
                 <form>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 form-felder-ein">
+                    <div className="form-felder-ein">
                         <input type="text" /><br />
                         <label>Vollständiger Name</label><br />
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 form-felder-ein">
+                    <div className="form-felder-ein">
                         <input type="text" /><br />
                         <label>Vollständiger Name</label><br />
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 form-felder-ein">
+                    <div className="form-felder-ein">
                         <input type="text" /><br />
                         <label>Vollständiger Name</label><br />
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 form-felder-ein">
+                    <div className="form-felder-ein">
                         <input type="text" /><br />
                         <label>Vollständiger Name</label><br />
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 form-felder-ein">
+                    <div className="form-felder-ein">
                         <textarea></textarea><br />
                         <label>Möchtest Du uns noch etwas mitteilen?</label>
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div className="">
                         <input className="input-button" type="submit" value="Submit" />
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <div className="">
                         <label><input type="checkbox" /> Ich möchte eine Antwort per Mail.</label>
                         <label><input type="checkbox" />Ich möchte zurückgerufen werden.</label>
                     </div>
