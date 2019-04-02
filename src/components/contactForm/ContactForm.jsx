05a07772d7b90858b035cd1b2a3e562027ba4c70
dennispio <dennis.piotrowski@web.de>
2019-04-02 19:07:07 +0200

    Font eingebunden als Globale

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -6,44 +6,50 @@ const ContactForm = () => (
         <div className="form-contacthead col-md-8 col-md-offset-2  col-xs-6 col-xs-offset-1">
             <p> <b>Unverbindlich Anfragen</b></p>
         </div>
-        <form>
+        <form method="POST" action="https://formspree.io/hello@immajung.de">
             <div className="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
-                <input type="text" /><br />
+                <input type="text" name="Name" /><br />
                 <label>Vollständiger Name</label><br />
             </div>
             <div className="form-felder-container  col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
-                <input type="text" /><br />
+                <input type="text" name="Straße" /><br />
                 <label>Straße und Hausnummer (optional)</label><br />
             </div>
             <div className="form-felder-container  col-md-8 col-md-offset-2  col-xs-10 col-xs-offset-1">
                 <div className="e-mail-container col-md-6 col-xs-12">
-                    <input type="text" /><br />
+                    <input type="email" name="Email" /><br />
                     <label>E-Mail</label><br />
                 </div>
                 <div className="telefon-container col-md-6 col-xs-12">
-                    <input type="text" /><br />
+                    <input type="text" name="Telefon" /><br />
                     <label>Telefon (optional)</label><br />
                 </div>
             </div>
             <div className="form-felder-container  col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
-                <textarea /><br />
+                <textarea name="message" /><br />
                 <label>Möchtest Du uns noch etwas mitteilen?</label>
             </div>
             <div className="checkbox-flow col-md-8 col-md-offset-2  col-xs-10 col-xs-offset-1">
-                <input type="checkbox" id="mail" name="mail" value="mail" />
+                <input type="checkbox" id="mail" name="Mail" value="ja" />
                 <label for="mail">Ich möchte eine Antwort per Mail.</label>
-                <input type="checkbox" id="rueckruf" name="rueckruf" value="rueckruf" />
+                <input type="checkbox" id="rueckruf" name="Rueckruf" value="ja" />
                 <label for="rueckruf">Ich möchte zurückgerufen werden.</label>
             </div>
             <div className="checkbox-column col-md-8 col-md-offset-2  col-xs-10 col-xs-offset-1">
-                <input type="checkbox" id="datenschutz" name="datenschutz" value="datenschutz" />
+                <input type="checkbox" id="datenschutz" name="Datenschutz" value="ja" />
                 <label for="datenschutz">Ich stimme den Datenschutzbestimmungen zu. <br /> Deine Daten werden nicht an Dritte weitergegeben.</label>
-                <input type="checkbox" id="newsletter" name="newsletter" value="newsletter" />
+                <input type="checkbox" id="newsletter" name="Newsletter" value="ja" />
                 <label for="newsletter">Ich möchte den Newsletter erhalten. <br /> Jederzeit abstellbar.</label>
             </div>
             <div className="col-md-3 col-md-offset-2  col-xs-10 col-xs-offset-1">
                 <input className="input-button" type="submit" value="Anfrage abschicken" />
             </div>
+        </form>
+
+        <form method="POST" action="https://formspree.io/hello@immajung.de">
+            <input type="email" name="email" placeholder="Your email" />
+            <textarea name="message" placeholder="Test Message"></textarea>
+            <button type="submit">Send Test</button>
         </form>
     </div >
 )
