49e2bf6adc56fda26f68b611f5f0e9202c40b1dd
Karim Ould Mahieddine <karimouldmahieddine@karims-mbp.speedport_w_724v_09011603_05_017>
2019-04-02 21:37:08 +0200

    carousel fix

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,109 +1,111 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Slider from 'react-slick';
+import right from '../../img/chevron-right.svg';
+import left from '../../img/chevron-left.svg';
 import './carousel.scss';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import "../../constants/style/variables.scss";
 
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style }}
+      onClick={onClick}
+      onKeyPress={onClick}
+      role="presentation"
+    >
+      <img src={right} alt="right arrow" />
+    </div>
+  );
+}
 
-
-
-function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "red" }}
-        onClick={onClick}
-      />
-    );
-  }
-  
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-      />
-    );
-  }
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style }}
+      onClick={onClick}
+      onKeyPress={onClick}
+      role="presentation"
+    >
+      <img src={left} alt="left arrow" />
+    </div>
+  );
+}
 
 export default class Carousel extends Component {
-    render() {
-      const settings = {
-        dots: true,
-        infinite: true,
-        autoplay:true,
-        nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        adaptiveHeight: true,
-        draggable: false,
-       
-      };
-      return (
-        <div className="container"> 
-        
-        <div className="homepageText-content">
-        <button className="slick-prev left-slick" type="button"/>
-        <button className="slick-prev right-slick" type="button"/>
-          <Slider {...settings} >
-         
-            <div >
-        
-                <div className="titelCarousel hompageText-title ">
-                  <h1>1. Nach 18 keine Kohlenhydrate mehr</h1>
-                  
-              </div>
-
-              <div className="linkerText hompageText-text">
-                  <p>linker text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
-              </div>
-              <div className="rechterText hompageText-text">
-                  <p>rechter text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
-              </div>
-                
-              
-              
-              
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      pauseOnHover: true,
+      adaptiveHeight: true,
+      draggable: true,
+      nextArrow: <NextArrow className="preArrow" />,
+      prevArrow: <PrevArrow className="nextArrow" />,
+    };
+    return (
+      <div className="container fact-carousel-container">
+        <Slider {...settings}>
+          <div className="slide container">
+            <div className="titelCarousel">
+              <h1>1. Nach 18 keine Kohlenhydrate mehr</h1>
             </div>
-
-            <div>
-            <div className="titelCarousel hompageText-title ">
-                  <h1>2. Nach 18 keine Kohlenhydrate mehr</h1>
+            <div className="row">
+              <div className="col-xs-12 col-sm-6 col-lg-6">
+                <p>
+                  linker text das ist der test für die linke seite responsiv
+                  muss es auch sein mak schauen was noch so kommt
+                </p>
               </div>
-
-              <div className="linkerText hompageText-text">
-                  <p>linker text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
-              </div>
-              <div className="rechterText hompageText-text">
-                  <p>rechter text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
+              <div className="col-xs-12 col-sm-6 col-lg-6">
+                <p>
+                  rechter text das ist der test für die linke seite responsiv
+                  muss es auch sein mak schauen was noch so kommt
+                </p>
               </div>
             </div>
-
-            <div>
-            <div className="titelCarousel hompageText-title ">
-                  <h1>3. Nach 18 keine Kohlenhydrate mehr</h1>
-              </div>
-
-              <div className="linkerText hompageText-text">
-                  <p>linker text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
-              </div>
-              <div className="rechterText hompageText-text">
-                  <p>rechter text das ist der test für die linke seite responsiv muss es auch sein mak schauen was noch so kommt</p>
-              </div>
-            </div>
-            
-          </Slider>
           </div>
-        </div>
-      );
-    }
+          <div className="slide container">
+            <div className="titelCarousel">
+              <h1>2. Nach 18 keine Kohlenhydrate mehr</h1>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-sm-6 col-lg-6">
+                <p>
+                  linker text das ist der test für die linke seite responsiv
+                  muss es auch sein mak schauen was noch so kommt
+                </p>
+              </div>
+              <div className="col-xs-12 col-sm-6 col-lg-6">
+                <p>
+                  rechter text das ist der test für die linke seite responsiv
+                  muss es auch sein mak schauen was noch so kommt
+                </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    );
   }
+}
 
-  
+NextArrow.propTypes = {
+  className: PropTypes.any,
+  style: PropTypes.any,
+  onClick: PropTypes.any,
+};
+
+PrevArrow.propTypes = {
+  className: PropTypes.any,
+  style: PropTypes.any,
+  onClick: PropTypes.any,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,10 +26,8 @@ const IndexPage = props => {
       </Helmet>
       <VideoPlayer />
       <HomepageText />
-      
       <Countdown date={countdown} />
-      <Carousel/>
-      
+      <Carousel />
       {/* eslint-disable-next-line */}
       {referenzen.map(({ node: referenz }, index) => {
         return referenz.frontmatter.show_homepage ? (
