4539a97773cfe00ab1cf22ad14d735dc20c619bd
Karim Ould Mahieddine <karimouldmahieddine@Karims-MacBook-Pro.local>
2019-04-15 14:34:57 +0200

    content fix

--- src/components/text/HomepageText.jsx
+++ src/components/text/HomepageText.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import './homepageText.scss';
+import PropTypes from 'prop-types';
 
-const HomepageText = () => (
+const HomepageText = ({ heading, text }) => (
   <div className="container-full homepageText-container">
     <div className="container">
       <div className="homepageText-content">
         <div className="hompageText-title">
-          <h1>
-            Hier kommt vielleicht der Text zu dem Bild oben: Eure Philosophie?
-          </h1>
+          <h1>{heading}</h1>
         </div>
         <div className="hompageText-text">
-          <p>
-            Die individuelle Ernährungsberatung ist eine auf Dich abgestimmte
-            Beratung. Das heißt konkret: Wir passen uns auf Dich an und nicht Du
-            auf uns. Du sagst uns was Du willst und wir sagen Dir nicht was Du
-            musst. musst. In Zusammenarbeit erstellen wir ein auf Dich
-            abgestimmtes Konzept, das Dir auf dem Weg zu deinem Ziel helfen
-            wird. Deine Ziele könnten sein:
-          </p>
+          <p>{text}</p>
         </div>
       </div>
     </div>
@@ -26,3 +18,8 @@ const HomepageText = () => (
 );
 
 export default HomepageText;
+
+HomepageText.propTypes = {
+  heading: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
