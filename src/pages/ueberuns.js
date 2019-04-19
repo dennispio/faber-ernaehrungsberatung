e936f90f070e98ce3c9d141d0f1ed18151495612
Ilias Zales <zales.ilias@gmail.com>
2019-04-20 00:12:44 +0200

    text-responsiv

--- src/pages/ueberuns.js
+++ src/pages/ueberuns.js
@@ -11,14 +11,50 @@ const Ueberuns = props => {
       <Layout  pageName="ueberuns"
       title="uberuns title"
       description="ueberuns desc">
-       <div id="ueberunsContainer" className="container-full">
-
-        <h>text zum guckdn</h>
-       </div>
-       <div id="containerText">
-
-       </div>
-      </Layout>
+      <div className="container-full ueberUnsContainer">
+       <div id="ueberunsContainerImg" />   
+       <div className="container ueberUnsContent">
+        <div className="titelUber"> 
+            <h1 className="texttitel">Ueber Uns</h1>
+        </div>
+            <div className="row">
+            
+                <div className="col-xs-12 col-sm-6 col-lg-5 ">
+                <h2 className="titelzwei">Unsere Entstehung und Philosophie</h2>
+                  <p className="textUber">text um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier 
+                      stehen kanntext um zu sehen was hier stehen kann text um zu sehtext um zu sehen was hier stehen kannen 
+                      was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext 
+                      um zu sehen was hier stehen kanntext um zu sehen was hier 
+                      stehen kanntext um zu sehen was hier stehen kann text um zu sehtext um zu sehen was hier stehen kannen 
+                      was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier 
+                      stehen kanntext um zu sehen was hier stehen kann text um zu sehtext um zu sehen was hier stehen kannen 
+                      was hier stehen hier stehen kannstehen kanntext um zu sehen was hier stehen kann text um zu sehtext um zu sehen was hier stehen kannen 
+                      was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier 
+                      stehen 
+                  </p>
+                </div>
+            <div className="textNamen">
+                <div className="col-xs-12 col-sm-6 col-lg-5 ">
+                <h2 className="titeldrei"> Daniel Faber</h2>
+                  <p className="textUber">Max Faber
+                      hex ho das isz der text hex ho das isz der text 
+                      hex ho das isz der text hex ho das isz der text 
+                      hex ho das isz der text hex ho das isz der text 
+                      hex ho das isz der text hex ho das isz der text 
+                  </p>
+                  <h2 className="titeldrei">Maxim Faber</h2>
+                  <p className="textUber">KP Faber
+                      hex ho das isz der text hex ho das isz der text 
+                      hex ho das isz der text hex ho das isz der text
+                      hex ho das isz der text hex ho das isz der text 
+                      hex ho das isz der text hex ho das isz der text  
+                  </p>
+                </div>
+             </div>
+            </div>  
+        </div>
+    </div>
+</Layout>
     );
   };
   export default Ueberuns;
