51c8847de08ed9fb89fd6928c2d5cf6c1f610991
Ilias Zales <zales.ilias@gmail.com>
2019-04-26 22:56:18 +0200

    first-changes

--- src/pages/ueberuns.js
+++ src/pages/ueberuns.js
@@ -3,6 +3,8 @@ import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Layout from '../components/layouts/Layout';
 import '../constants/style/ueberuns.scss';
+import bubbleOne from '../img/bubbles/1FBR-bubble-RGB.svg';
+
 
 const Ueberuns = props => {
     const { data } = props;
@@ -12,15 +14,17 @@ const Ueberuns = props => {
       title="uberuns title"
       description="ueberuns desc">
       <div className="container-full ueberUnsContainer">
+     
        <div id="ueberunsContainerImg" />   
        <div className="container ueberUnsContent">
-        <div className="titelUber"> 
-            <h1 className="texttitel">Ueber Uns</h1>
-        </div>
+       
             <div className="row">
-            
-                <div className="col-xs-12 col-sm-6 col-lg-5 ">
-                <h2 className="titelzwei">Unsere Entstehung und Philosophie</h2>
+            <div className="titelUber"> 
+            <h1 className="texttitel ">Ueber Uns</h1>
+        </div>
+             
+                <div className="col-xs-12 col-sm-6 col-lg-5  ">
+                <h2 className="titelzwei farbverlauf">Unsere Entstehung und Philosophie</h2>
                   <p className="textUber">text um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier 
                       stehen kanntext um zu sehen was hier stehen kann text um zu sehtext um zu sehen was hier stehen kannen 
                       was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext um zu sehen was hier stehen kanntext 
@@ -35,6 +39,7 @@ const Ueberuns = props => {
                 </div>
             <div className="textNamen">
                 <div className="col-xs-12 col-sm-6 col-lg-5 ">
+                <h1 className="titelunten farbvelauf"> Das sind wir</h1>
                 <h2 className="titeldrei"> Daniel Faber</h2>
                   <p className="textUber">Max Faber
                       hex ho das isz der text hex ho das isz der text 
