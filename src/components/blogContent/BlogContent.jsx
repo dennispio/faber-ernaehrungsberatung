22aa76ff287ceeb308e951c44ec647ba5213027e
Karim Ould Mahieddine <karimouldmahieddine@karims-mbp.speedport_w_724v_09011603_05_017>
2019-05-30 14:48:40 +0200

    service template fix, nav fix

--- src/components/blogContent/BlogContent.jsx
+++ src/components/blogContent/BlogContent.jsx
@@ -16,39 +16,39 @@ const BlogContent = ({
   blog,
   service,
 }) => (
-  <div className="container-full">
-    <div className="subpage-content-container">
-      {fluid ? (
-        <div className="blog-page-image-container">
-          <Img
-            alt={alt}
-            fluid={fluid}
-            style={{
-              left: 0,
-              top: 0,
-              width: '100%',
-              height: '100%',
-            }}
-          />
+    <div className="container-full">
+      <div className={service ? "subpage-service-container" : "subpage-content-container"}>
+        {fluid ? (
+          <div className="blog-page-image-container">
+            <Img
+              alt={alt}
+              fluid={fluid}
+              style={{
+                left: 0,
+                top: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          </div>
+        ) : null}
+        <div className="blog-info row">
+          <div className="col-xs-8 col-sm-10 col-lg-10">{cat}</div>
+          <div className="col-xs-4 col-sm-2 col-lg-2 date">{date}</div>
         </div>
-      ) : null}
-      <div className="blog-info row">
-        <div className="col-xs-8 col-sm-10 col-lg-10">{cat}</div>
-        <div className="col-xs-4 col-sm-2 col-lg-2 date">{date}</div>
+        <div
+          className={service ? 'form-felder' : 'blog-container'}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        {share !== false ? (
+          <div className="row">
+            <ShareButton />
+            <Newsletter />
+          </div>
+        ) : null}
       </div>
-      <div
-        className={service ? 'form-felder' : 'blog-container'}
-        dangerouslySetInnerHTML={{ __html: post.html }}
-      />
-      {share !== false ? (
-        <div className="row">
-          <ShareButton />
-          <Newsletter />
-        </div>
-      ) : null}
     </div>
-  </div>
-);
+  );
 
 BlogContent.propTypes = {
   title: PropTypes.string.isRequired,
