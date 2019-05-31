9d2bc4cea925e4493d0843394a44120fc9faffd8
Karim Ould Mahieddine <karimouldmahieddine@karims-mbp.speedport_w_724v_09011603_06_003>
2019-05-31 15:10:23 +0200

    style style style

--- src/components/blogPostCard/BlogPostPageCard.jsx
+++ src/components/blogPostCard/BlogPostPageCard.jsx
@@ -10,18 +10,32 @@ import './blogPostCard.scss';
 //   }
 // };
 
+const calcTruncate = title => {
+  const count = title.split('').length;
+  if (count >= 0 && count <= 11) {
+    return 10;
+  }
+  if (count > 10 && count <= 21) {
+    return 8;
+  }
+  if (count > 20 && count <= 33) {
+    return 7;
+  }
+  return 6;
+};
+
 const BlogPostPageCard = ({ category, date, title, text, link, index }) => (
   <div className={`grid-item blogPostCard overviewCard grid-item--${index}`}>
     <div className="blogPost-head-container">
       <span className="cat">{category}</span>
       <span className="date">{date}</span>
     </div>
-    <div className="blogPost-title-container">
-      <h5>{title}</h5>
+    <div className="blogPost-title-container test">
+      <h3>{title}</h3>
     </div>
     <div className="blogPost-content-container">
       <Truncate
-        lines={5}
+        lines={calcTruncate(title)}
         ellipsis={
           <span>
             ... <br /> <Link to={link}> mehr lesen</Link>
