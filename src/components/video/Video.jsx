635e98b4053efcb9600218d0574e2b09498072b3
dennispio <dennis.piotrowski@web.de>
2019-05-31 12:19:28 +0200

    VideoFormatTry

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './videoPlayer.scss';
-import video from '../../img/video.mp4';
+import video from '../../img/FBR-countdown_animation-desktop-K0-C.mp4';
+import videoMobile from '../../img/FBR-countdown_animation-mobil-K0-D.mp4';
+
 /* ToDo: Mit CMS Verknüpfen and Play button on mobile */
 const VideoPlayer = () => (
   <div className="container-full">
@@ -8,9 +10,15 @@ const VideoPlayer = () => (
       <h2>Lebensqualität <br /> steigern - ohne <br /> zu verzichten</h2>
     </div>
     <video id="background-video" playsInline muted autoPlay>
-      <source src={video} type="video/mp4" />
-      <source src={video} type="video/ogg" />
+      <source src={video} media="all and (min-width: 576px)" type="video/mp4" />
+      <source src={video} media="all and (min-width: 576px)" type="video/ogg" />
       Your browser does not support the video tag.
+      <source src={videoMobile} media="all and (max-width: 575px)" type="video/mp4" />
+      <source
+        src={videoMobile}
+        media="all and (max-width: 576px)"
+        type="video/ogg"
+      />
     </video>
   </div>
 );
