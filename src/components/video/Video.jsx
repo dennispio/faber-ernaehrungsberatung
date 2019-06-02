d5b80408ff187489a61a452356453b8f22de4993
dennispio <dennis.piotrowski@web.de>
2019-06-02 14:07:30 +0200

    Video Anpassung Tablet / Shadow - Blog und Angebot

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -10,10 +10,10 @@ const VideoPlayer = () => (
       <h2>Lebensqualität <br /> steigern - ohne <br /> zu verzichten</h2>
     </div>
     <video id="background-video" playsInline muted autoPlay>
-      <source src={video} media="all and (min-width: 576px)" type="video/mp4" />
-      <source src={video} media="all and (min-width: 576px)" type="video/ogg" />
+      <source src={video} media="all and (min-width: 1200px)" type="video/mp4" />
+      <source src={video} media="all and (min-width: 1200px)" type="video/ogg" />
       Your browser does not support the video tag.
-      <source src={videoMobile} media="all and (max-width: 575px)" type="video/mp4" />
+      <source src={videoMobile} media="all and (max-width: 1199px)" type="video/mp4" />
       <source
         src={videoMobile}
         media="all and (max-width: 576px)"
