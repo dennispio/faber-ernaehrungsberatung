1c376d7f300e463cd8c815a3c0102289b253444f
dennispio <dennis.piotrowski@web.de>
2019-06-17 12:45:28 +0200

    Favicon add

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -7,6 +7,7 @@ import { Link } from 'gatsby';
 import Footer from '../footer/Footer';
 import Navigation from '../navigation/Navigation';
 import logo from '../../img/FBR-logo-sw.svg';
+import favicon from '../../img/FBR-Logo-Favicon-RGB.png';
 
 const TemplateWrapper = ({
   children,
@@ -16,35 +17,37 @@ const TemplateWrapper = ({
   type,
   image,
 }) => (
-  <div>
-    <Helmet>
-      <html lang="de" />
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:type" content={type || 'website'} />
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content="/" />
-      <meta property="og:image" content={image || logo} />
-    </Helmet>
-    <Navigation pageName={pageName} />
-    <div>{children}</div>
-    <CookieConsent
-      cookieName="maxim-ernaehrungsberatung-cookie"
-      style={{ background: 'linear-gradient(to right,  #140A37, #E61E0A)' }}
-      location="bottom"
-      buttonText="Ich stimme zu."
-    >
-      <span style={{ fontSize: '15px' }}>
-        Diese Website verwendet Cookies – nähere Informationen dazu und zu Ihren
+    <div>
+      <Helmet>
+        <html lang="de" />
+        <title>{title}</title>
+        <link rel="shortcut icon" href={favicon} type="image/png" />
+        <link rel="icon" href={favicon} type="image/png" />
+        <meta name="description" content={description} />
+        <meta property="og:type" content={type || 'website'} />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="/" />
+        <meta property="og:image" content={image || logo} />
+      </Helmet>
+      <Navigation pageName={pageName} />
+      <div>{children}</div>
+      <CookieConsent
+        cookieName="maxim-ernaehrungsberatung-cookie"
+        style={{ background: 'linear-gradient(to right,  #140A37, #E61E0A)' }}
+        location="bottom"
+        buttonText="Ich stimme zu."
+      >
+        <span style={{ fontSize: '15px' }}>
+          Diese Website verwendet Cookies – nähere Informationen dazu und zu Ihren
         Rechten als Benutzer finden Sie in unserer{' '}
-        <Link to="/legal/datenschutz">Datenschutzerklärung</Link> am Ende der
-        Seite. Klicken Sie auf „Ich stimme zu“, um Cookies zu akzeptieren und
-        direkt unsere Website besuchen zu können.
+          <Link to="/legal/datenschutz">Datenschutzerklärung</Link> am Ende der
+          Seite. Klicken Sie auf „Ich stimme zu“, um Cookies zu akzeptieren und
+          direkt unsere Website besuchen zu können.
       </span>
-    </CookieConsent>
-    <Footer />
-  </div>
-);
+      </CookieConsent>
+      <Footer />
+    </div>
+  );
 
 TemplateWrapper.propTypes = {
   children: PropTypes.node.isRequired,
