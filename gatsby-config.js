1c9ecf9baefd595c97df6fc96b47f9ea23999eae
dennispio <dennis.piotrowski@web.de>
2019-08-08 00:55:19 +0200

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         trackingId: "UA-145257759-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
