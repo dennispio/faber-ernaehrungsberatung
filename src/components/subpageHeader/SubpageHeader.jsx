8121a624a0e6233a4ca2554a07b2e99c96e39165
Karim Ould Mahieddine <karim_om@me.com>
2019-08-12 11:54:57 +0200

    quick fix

--- src/components/subpageHeader/SubpageHeader.jsx
+++ src/components/subpageHeader/SubpageHeader.jsx
@@ -21,7 +21,7 @@ const SubpageHeader = ({ title, text, sitename, blog }) => (
       alt="hintergrund bubble links"
     />
     <div className="subpage-title-container farbverlauf">
-      {sitename && sitename !== 'blog' ? (
+      {sitename && title !== 'Blog' ? (
         <h1 className={`farbverlauf ${sitename}`}>{title}</h1>
       ) : (
         <h2 className={`farbverlauf ${sitename}`}>{title}</h2>
