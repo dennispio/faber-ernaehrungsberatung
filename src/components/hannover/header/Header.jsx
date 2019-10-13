3f6c99b24b64ae3bd2b1e7b4b98327d2f5ff0304
dennispio <dennis.piotrowski@web.de>
2019-10-13 11:15:04 +0200

    CSSGRID

--- src/components/hannover/header/Header.jsx
+++ src/components/hannover/header/Header.jsx
@@ -1,23 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import "./header.scss";
+import './header.scss';
 
-const Header = () => {
-  return (
-    <div className="header-han-container">
-      <div className="header-han-text">
-        <h1>
-          Warum verzichten,
-          <br /> wenn du nicht musst ?
-        </h1>
-        <h5>
-          Iss was du willst, wann du willst,
-          <br />
-          und werde so, wie du es willst.
-        </h5>
-      </div>
+const Header = () => (
+  <div className="header-han-container">
+    <div className="header-han-grid">
+      <h1 className="header-han-text-h1">
+        Warum verzichten,
+        <br /> wenn du nicht musst ?
+      </h1>
+      <h2 className="header-han-text-h2">
+        Iss was du willst, wann du willst, und werde so, wie du es willst.
+      </h2>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
