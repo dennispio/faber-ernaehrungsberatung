8d5627e3a62f5592756ebf68d2dcbec774c812c0
dennispio <dennis.piotrowski@web.de>
2019-10-16 14:21:49 +0200

    Header Ready

--- src/components/hannover/details/Details.jsx
+++ src/components/hannover/details/Details.jsx
@@ -6,16 +6,49 @@ const Details = () => (
   <div className="container-full details-container">
     <div className="details-container-img" />
     <div className="grid-oben">
-      <h3>Hannover ist die Abnehmstadt Nr.1</h3>
+      <h3 className="farbverlauf">Hannover ist die Abnehmstadt Nr.1</h3>
       <p>
         Lorem 111 ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
         nostrum corrupti. Dolorem quidem incidunt dignissimos mollitia
         similique, nam velit sit modi. Et iusto nam labore. Error labore
-        corporis voluptatibus hic!
+        corporis voluptatibus hic! Lorem 111 ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium, nostrum corrupti. Dolorem quidem incidunt
+        dignissimos mollitia similique, nam velit sit modi. Et iusto nam labore.
+        Error labore corporis voluptatibus hic!
       </p>
     </div>
-    <div className="grid-mitte-links">
+    <div className="grid-mitte-links farbverlauf">
       <h3>Drei gute Gründe die für uns sprechen:</h3>
+    </div>
+    <div className="grid-links">
+      <div className="grund-container">
+        <span className="big-number-left blau">1</span>
+        <p className="p-container-left">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes.
+        </p>
+      </div>
+    </div>
+    <div className="grid-rechts">
+      <div className="grund-container">
+        <span className="big-number-right blau">2</span>
+        <p className="p-container-right">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes.
+        </p>
+      </div>
+    </div>
+    <div className="grid-bottom">
+      <div className="grund-container">
+        <span className="big-number-bottom blau">3</span>
+        <p className="p-container-bottom">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes.
+        </p>
+      </div>
     </div>
   </div>
 );
--- src/components/hannover/header/Header.jsx
+++ src/components/hannover/header/Header.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import './header.scss';
 
 const Header = () => (
-  <div className="header-han-container">
-    <div className="header-han-grid">
-      <h1 className="header-han-text-h1">
-        Warum verzichten,
-        <br /> wenn du nicht musst ?
-      </h1>
-      <h2 className="header-han-text-h2">
-        Iss was du willst, wann du willst, und werde so, wie du es willst.
-      </h2>
+  <div className="container-full header-han-container-img">
+    <div className="container">
+      <div className="header-han-container">
+        <div className="header-han-grid">
+          <h1 className="header-han-text-h1">
+            Faber Ernährungsberatung in Hannover - Langenhagen und Umgebung
+          </h1>
+          <h2 className="header-han-text-h2">
+            Iss was du willst, wann du willst, und werde so, wie du es willst.
+          </h2>
+        </div>
+      </div>
     </div>
   </div>
 );
