a0616de4fd271261c7fe902477f7c608f7674199
karimould <karim_om@me.com>
2019-12-02 12:45:53 +0100

    added text to Certified component

--- src/components/hannover/state-certified/Certified.jsx
+++ src/components/hannover/state-certified/Certified.jsx
@@ -9,15 +9,12 @@ const Certified = () => {
       <div className="col-md-5 col-xs-12">
         <h2 className="farbverlauf">Wir sind staatlich geprüft</h2>
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
+          Eine professionelle und individuelle Ernährungsberatung bildet die
+          Grundlage einer erfolgreichen Zusammenarbeit im Ernährungscoaching.
+          Mit unserem ausgebildeten und staatlich geprüften Ernährungsberater
+          durch den TÜV SÜD garantieren wir einen hohen Qualitätsstandard unter
+          stetiger Berücksichtigung der aktuellen wissenschaftlichen
+          Erkenntnisse.
         </p>
       </div>
       <div className="col-md-1" />
